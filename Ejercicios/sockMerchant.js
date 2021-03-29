397de13acb1ce25e7877accc5580662805001304
carrauKrizaj <81568779+carrauKrizaj@users.noreply.github.com>
2021-03-29 12:07:28 -0300

    resol_ejer3

--- Ejercicios/sockMerchant.js
+++ Ejercicios/sockMerchant.js
@@ -10,10 +10,33 @@
  * @param {Array} colorSocks 
  * @return {Number} 
  */
-const sockMerchant = function(colorSocks){
+ const sockMerchant = function(colorSocks){
 
-}
-
-// TESTS
-console.log(sockMerchant([1,2,1,2,1,3,2]) === 2);
-console.log(sockMerchant([10,20,20,10,10,30,50,10,20]) === 3);
+    let parAux = -1;
+    let contador = 0;
+    
+    while(colorSocks.length > 1){
+    
+        let parBusc = -1;
+        let hayPar = false;
+        let indice = 0;
+        parAux = colorSocks.shift();
+    
+        while(!hayPar && indice <= colorSocks.length){
+    
+            parBusc = colorSocks[indice];
+            hayPar = parAux === parBusc;
+    
+            if(hayPar){
+                colorSocks.splice(indice,1);
+                contador++
+            };        
+            indice++;
+        };
+    };
+    return contador;
+    };
+    
+    // TESTS
+    console.log(sockMerchant([1,2,1,2,1,3,2]) === 2);
+    console.log(sockMerchant([10,20,20,10,10,30,50,10,20]) === 3);
