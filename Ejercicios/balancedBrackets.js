faffc11fd7b0651299fc000bee8b71819c6661a1
carrauKrizaj <81568779+carrauKrizaj@users.noreply.github.com>
2021-03-29 12:05:59 -0300

    resolucion_ejer1

--- Ejercicios/balancedBrackets.js
+++ Ejercicios/balancedBrackets.js
@@ -10,11 +10,83 @@
 
 /**
  * 
+ * @param {string} c1
+ * @param {string} c2
+ * @return {boolean}
+ */
+ const evaluarChar = function(c1,c2){
+    const parA = '(';
+    const corA = '[';
+    const lavA = '{';
+    const parB = ')';
+    const corB = ']';
+    const lavB = '}';
+
+    let retorno = -1;
+
+    switch (c1) {
+        case parA:
+            if(c2===parB){
+                retorno = 1;
+            } else if(c2===corA || c2 === lavA){
+                retorno = 0;
+            };
+        case corA:
+            if(c2===corB){
+                retorno = 1;
+            } else if(c2===parA || c2 === lavA){
+                retorno = 0;
+            };
+        case lavA:
+            if(c2===lavB){
+                retorno = 1;
+            } else if(c2===parA || c2 === corA){
+                retorno = 0;
+            };            
+        default: 
+            retorno = -1;
+    }
+    return retorno;
+ };
+
+
+/**
+ * 
  * @param {string} s 
  * @return {boolean}
  */
 const isValid = function(s){
+    
+    const esValido = 0;
+    const esValYPar = 1;
+    const esInvalido = -1;
+    let continua = true;
+    let retorno = true;
+    let cadenAux = s.split();
+    let indice = 0;
+    let charAux = -1;
+    let charB = -1;
+    let compara = -1;
 
+    while(cadenAux.length > 1 && retorno == true){
+        charAux = cadenAux.shift();
+
+        while(continua){
+            charB = cadenAux[indice];
+            compara = evaluarChar(charAux,charB);
+            if(compara === esValYPar){
+                cadenAux.splice(indice,1);
+                continua = false;
+            } else if (compara === esValido){
+                continua = true;
+            } else if (compara === esInvalido){
+                continua = false;
+                retorno = false;
+            };
+
+        };
+    };
+    return retorno;
 };
 
 // TESTS
